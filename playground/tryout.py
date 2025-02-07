8196bd2ebdd134ad4ad842557095f545200102a2
Nuno Rocha <nuno.rocha@ratepay.com>
2025-02-07 17:15:16 +0100

    Try some examples

--- playground/tryout.py
+++ playground/tryout.py
@@ -25,7 +25,7 @@ def configure_dspy(
     yield
     print()
     try:
-        trace[0][0].save(path=str(Path(__file__).parent / 'out' / f'log_{datetime.now().timestamp()}.json'))
+        trace[0][0].save(path=str(Path(__file__).parents[1] / 'out' / f'log_{datetime.now().timestamp()}.json'))
     except Exception as ex:
         print(ex)
 
@@ -35,6 +35,7 @@ def configure_dspy(
 
     print(json.dumps(lm.history, indent=2, default=str))
 
+
 def test_math():
     math = dspy.ChainOfThought("question -> answer: float")
     math(question="Two dice are tossed. What is the probability that the sum equals two?")
@@ -42,7 +43,7 @@ def test_math():
 
 def test_rag():
     def search_wikipedia(query: str) -> list[str]:
-        results = dspy.ColBERTv2(url='http://20.102.90.50:2017/wiki17_abstracts')(query, k=3)
+        results: list[dict] = dspy.ColBERTv2(url='http://20.102.90.50:2017/wiki17_abstracts')(query, k=3)
         return [x['text'] for x in results]
 
     rag = dspy.ChainOfThought('context, question -> response')
@@ -80,3 +81,53 @@ def test_extraction():
     print(response.title)
     print(response.headings)
     print(response.entities)
+
+
+def test_agents():
+    def evaluate_math(expression: str):
+        return dspy.PythonInterpreter({}).execute(expression)
+
+    def search_wikipedia(query: str):
+        results: list[dict] = dspy.ColBERTv2(url='http://20.102.90.50:2017/wiki17_abstracts')(query, k=3)
+        return [x['text'] for x in results]
+
+    react = dspy.ReAct("question -> answer: float", tools=[evaluate_math, search_wikipedia])
+
+    pred = react(question="What is 9362158 divided by the year of birth of David Gregory of Kinnairdy castle?")
+    print(pred.answer)
+
+
+def test_multistage_pipeline():
+    class Outline(dspy.Signature):
+        """Outline a thorough overview of a topic."""
+
+        topic: str = dspy.InputField()
+        title: str = dspy.OutputField()
+        sections: list[str] = dspy.OutputField()
+        section_subheadings: dict[str, list[str]] = dspy.OutputField(desc="mapping from section headings to subheadings")
+
+    class DraftSection(dspy.Signature):
+        """Draft a top-level section of an article."""
+
+        topic: str = dspy.InputField()
+        section_heading: str = dspy.InputField()
+        section_subheadings: list[str] = dspy.InputField()
+        content: str = dspy.OutputField(desc="markdown-formatted section")
+
+    class DraftArticle(dspy.Module):
+        def __init__(self):
+            self.build_outline = dspy.ChainOfThought(Outline)
+            self.draft_section = dspy.ChainOfThought(DraftSection)
+
+        def forward(self, topic):
+            outline = self.build_outline(topic=topic)
+            sections = []
+            for heading, subheadings in outline.section_subheadings.items():
+                section, subheadings = f"## {heading}", [f"### {subheading}" for subheading in subheadings]
+                section = self.draft_section(topic=outline.title, section_heading=section, section_subheadings=subheadings)
+                sections.append(section.content)
+            return dspy.Prediction(title=outline.title, sections=sections)
+
+    draft_article = DraftArticle()
+    article = draft_article(topic="World Cup 2022")
+    print(article)
