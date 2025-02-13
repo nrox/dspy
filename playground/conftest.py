da78237c6aa6f8656672b710802ec389f7a79e09
Nuno Rocha <nunuroxa@gmail.com>
2025-02-13 20:20:51 +0100

    Add test basic rag

--- playground/conftest.py
+++ playground/conftest.py
@@ -9,20 +9,20 @@ from blib2to3.pgen2.driver import contextmanager
 from pydantic import BaseModel
 
 import dspy
+from dspy.clients.base_lm import GLOBAL_HISTORY
 
 trace = []
 
 MODEL = [
     "ollama_chat/deepseek-r1:8b-llama-distill-q8_0",
-    'ollama_chat/llama3.1:8b-instruct-q8_0',
-    'ollama_chat/deepscaler:1.5b-preview-q8_0'
-][0]
+    "ollama_chat/llama3.1:8b-instruct-q8_0",
+    "ollama_chat/deepscaler:1.5b-preview-q8_0",
+    "ollama_chat/smollm2:1.7b-instruct-q8_0",
+    "ollama_chat/llama3.2:3b-instruct-q8_0"
+][-1]
 
 
-def get_lm(
-        model: str = MODEL,
-        api_key: str = "PROVIDER_API_KEY",
-        api_base: str = 'http://localhost:11434'):
+def get_lm(model: str = MODEL, api_key: str = "PROVIDER_API_KEY", api_base: str = "http://localhost:11434"):
     lm = dspy.LM(
         model=model,
         api_key=api_key,
@@ -30,14 +30,14 @@ def get_lm(
         max_tokens=8 * 1024,
         num_ctx=8 * 1024,
     )
-    print("configure llm ", model)
+    print("configure llm ", model, "\n", "=" * 50, "\n" * 2)
     return lm
 
 
 @contextmanager
 def conf_dspy():
-    start = re.sub(r'\W+', '_', datetime.now().isoformat())
-    folder = Path(__file__).parents[1] / 'out' / start
+    start = re.sub(r"\W+", "_", datetime.now().isoformat())
+    folder = Path(__file__).parents[1] / "out" / start
     os.makedirs(folder, exist_ok=True)
     lm = get_lm()
     trace.clear()
@@ -45,22 +45,30 @@ def conf_dspy():
     yield
     print()
     try:
-        trace[0][0].save(path=str(folder / 'save.json'))
+        if trace:
+            trace[0][0].save(path=str(folder / "save.json"))
     except Exception as ex:
         print(ex)
 
-    with open(folder / 'traces.txt', 'w', encoding='utf8') as fp:
+    with open(folder / "traces.txt", "w", encoding="utf8") as fp:
         for i in trace:
             for j in i:
                 fp.write(j.model_dump_json(indent=2) if isinstance(j, BaseModel) else str(j))
                 fp.write("\n")
             fp.write("\n")
 
-    with open(folder / 'history.json', 'w', encoding='utf8') as fp:
+    with open(folder / "history.json", "w", encoding="utf8") as fp:
         json.dump(lm.history, fp, indent=2, default=lambda v: v.model_dump() if isinstance(v, BaseModel) else str(v))
 
+    with open(folder / "global_history.json", "w", encoding="utf8") as fp:
+        json.dump(
+            GLOBAL_HISTORY, fp, indent=2, default=lambda v: v.model_dump() if isinstance(v, BaseModel) else str(v)
+        )
 
-@pytest.fixture(scope='function', autouse=True)
+    print("\n\n", "=" * 50, "\n" * 2)
+
+
+@pytest.fixture(scope="function", autouse=True)
 def configure_dspy():
     with conf_dspy():
         yield
---
+++ playground/test_play_rag.py
@@ -0,0 +1,94 @@
+import random
+
+import ollama
+import ujson
+
+import dspy
+from dspy.evaluate import SemanticF1
+from dspy.utils import download
+
+
+def test_basic_predict():
+    qa = dspy.Predict("question: str -> response: str")
+    response = qa(question="what are high memory and low memory on linux?")
+    print(response.response)
+
+
+def test_basic_cot():
+    cot = dspy.ChainOfThought("question -> response")
+    response = cot(question="should curly braces appear on their own line?")
+    print(response.response)
+
+
+def test_examples():
+    # Download question--answer pairs from the RAG-QA Arena "Tech" dataset.
+    download("https://huggingface.co/dspy/cache/resolve/main/ragqa_arena_tech_examples.jsonl")
+
+    with open("ragqa_arena_tech_examples.jsonl") as f:
+        data = [ujson.loads(line) for line in f]
+
+    data = [dspy.Example(**d).with_inputs("question") for d in data]
+
+    # Let's pick an `example` here from the data.
+    example = data[2]
+    print(example)
+
+    random.Random(0).shuffle(data)
+    scale = 10
+    trainset, devset, testset = data[:2*scale], data[2*scale:5*scale], data[500:1000]
+
+    # Instantiate the metric.
+    metric = SemanticF1(decompositional=True)
+
+    # Produce a prediction from our `cot` module, using the `example` above as input.
+    cot = dspy.ChainOfThought("question -> response")
+    pred = cot(**example.inputs())
+
+    # Compute the metric score for the prediction.
+    score = metric(example, pred)
+
+    print(f"Question: \t {example.question}\n")
+    print(f"Gold Response: \t {example.response}\n")
+    print(f"Predicted Response: \t {pred.response}\n")
+    print(f"Semantic F1 Score: {score:.2f}")
+
+    if False:
+        return
+
+    # Define an evaluator that we can re-use.
+    evaluate = dspy.Evaluate(devset=devset, metric=metric, num_threads=24,
+                            display_progress=True, display_table=2)
+
+    # Evaluate the Chain-of-Thought program.
+    evaluate(cot)
+
+def ollama_embed_fn(text: str):
+    embed_model = "nomic-embed-text:latest"
+    return ollama.embed(model=embed_model, input=text).embeddings
+
+
+def test_basic_rag():
+    class RAG(dspy.Module):
+        def __init__(self, search: dspy.retrievers.Embeddings):
+            self.respond = dspy.ChainOfThought('context, question -> response')
+            self.search = search
+
+        def forward(self, question):
+            context = self.search(question).passages
+            return self.respond(context=context, question=question)
+    
+    dataset_name = "ragqa_arena_tech_corpus.jsonl"
+    download(f"https://huggingface.co/dspy/cache/resolve/main/{dataset_name}")
+
+    max_characters = 6000  # for truncating >99th percentile of documents
+    topk_docs_to_retrieve = 5  # number of documents to retrieve per search query
+
+    with open("ragqa_arena_tech_corpus.jsonl") as f:
+        corpus = [ujson.loads(line)['text'][:max_characters] for line in f]
+        print(f"Loaded {len(corpus)} documents. Will encode them below.")
+
+    embedder = dspy.Embedder(ollama_embed_fn, batch_size=1)
+    search = dspy.retrievers.Embeddings(embedder=embedder, corpus=corpus, k=topk_docs_to_retrieve)
+    rag = RAG(search=search)
+    response = rag(question="what are high memory and low memory on linux?")
+    print(response)
